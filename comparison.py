cc1ef3bc37755d163ec7e84170b976921d4560f3
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2020-09-25 14:25:27 +0100

    comparison.py

--- comparison.py
+++ comparison.py
@@ -6,10 +6,7 @@ with open('Familial hypercholesterolaemia.tsv') as f_h_panel:
     for i in f_h_panel:
         f_h_panel_list.append(i)
     print(f_h_panel_list)
-    for row in f_h_panel_list:
+    for row in f_h_panel_list[1:]:
         first_word_list.append(row.split()[0])
-    print(len(f_h_panel_list))
-
 
 print(first_word_list)
-print
