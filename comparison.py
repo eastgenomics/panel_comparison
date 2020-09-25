62766d2e47727f722e368f5c3d56bcc12adaf9ec
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2020-09-25 16:30:12 +0100

    comparison.py

--- comparison.py
+++ comparison.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env/ python
 
-first_word_list = []
+genes_in_panel_app = []
 with open('Familial hypercholesterolaemia.tsv') as f_h_panel:
     f_h_panel_list = []
     for i in f_h_panel:
         f_h_panel_list.append(i)
     for row in f_h_panel_list[1:]:
-        first_word_list.append(row.split()[0])
+        genes_in_panel_app.append(row.split()[0])
 
-print(first_word_list)
+genes_in_lab_panel = []
+with open('gemini_panels_200522') as lab_panel:
+    lab_panel_list = []
+    for i in lab_panel:
+      lab_panel_list.append(i)
+    for row in lab_panel_list[1:]:
+        genes_in_lab_panel.append(row.split()[-1])
+
+genes_in_common = []
+for i in genes_in_panel_app:
+    if i in genes_in_lab_panel:
+        genes_in_common.append(i)
+
+print(genes_in_common)
