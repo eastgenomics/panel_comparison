295ab4a01684164e04aaf00e94b6d7482613c94e
Katherine Winfield <71272357+kjwinfield@users.noreply.github.com>
2021-03-12 11:16:48 +0000

    Update and rename test.txt to test.py
    testing comment additions for computing for bio essay

---
+++ test.py
@@ -0,0 +1,15 @@
+def creatinine_clearance_calculator(age, weight, sex, serum):
+    '''
+    function to generate creatinine clearance rate
+    parameters = age in years, weight in kg, sex, serum creatinine in micromol/L
+    returns = creatinine clearance rate in mL/minute
+    '''
+    if sex == 'm':
+        constant = 1.23
+        creatinine_clearance_rate = ((140 - int(age)) * int(weight) * constant) / int(serum)
+    elif sex == 'f':
+        constant = 1.04
+        creatinine_clearance_rate = ((140 - int(age)) * int(weight) * constant) / int(serum)
+    else:
+       creatinine_clearance_rate = 0
+    return creatinine_clearance_rate
